61fb6f101f29f4e428ca55faf82cf122548ae2bd
tidzi17 <tidzi15110@gmail.com>
2024-02-27 18:13:08 +0100

    contact page

--- src/components/contactcomponents/contactSection.jsx
+++ src/components/contactcomponents/contactSection.jsx
@@ -5,12 +5,12 @@ import ContactForm from './ContactForm';
 export default function ContactSection() {
   return (
     <section>
-        <div  className='w-10/12 h-[90vh] m-auto mb-10 flex flex-row items-center'>
-            <div className='w-1/2 h-full rounded-2xl rounded-image'>
+        <div  className='relative w-full  lg:w-10/12 h-[90vh] m-auto mb-10 flex flex-row items-center'>
+            <div className='relative w-full md:w-1/2 h-full z-10  rounded-2xl rounded-image'>
                 <img src={bg} alt='' className='w-[100%] h-[100%] object-cover rounded-2xl'/>
             </div>
-            <div className='w-1/2 h-full  flex flex-col justify-center px-5'>
-            <div className=' h-fit mb-7'>
+            <div className='absolute md:relative w-full md:w-1/2 h-full z-20 flex flex-col justify-center px-5'>
+            <div className=' h-fit mb-5'>
     <p className='text-xl font-junge'>Contact Us</p>
     <p className='text-3xl mb-2 font-italiana'>Get in Touch</p>
     <p className='text-lg font-junge text-zinc-700'>Have a question or feedback? Feel free to reach out to us! Our team is here to assist you with any inquiries and provide support. Whether it's a product inquiry, partnership opportunity, or just to say hello, we'd love to hear from you!</p>
--- src/components/mobileBottomBar.jsx
+++ src/components/mobileBottomBar.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 
 const MobileBottomBar = () => {
   return (
-    <div className='flex justify-between items-center py-5 bg-white px-4 w-full border-t-[1px] border-black z-40  '>
+    <div className='flex justify-between items-center py-4 bg-[#f4f0e5] px-4 w-full border-t-[1px] border-black z-40  '>
     <span className='text-2xl text-black'><CiUser /></span>
      <Link to='/wishlist'><span className='text-black text-2xl '><CiHeart /></span></Link>
     <Link to='/cart'> <span className='text-black text-2xl '><IoBagHandleOutline /></span></Link>
