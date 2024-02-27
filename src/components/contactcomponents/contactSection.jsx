a809647fe6b5766050787f9101a2ada1068c5577
tidzi17 <tidzi15110@gmail.com>
2024-02-27 18:17:58 +0100

    contact

--- src/components/contactcomponents/contactSection.jsx
+++ src/components/contactcomponents/contactSection.jsx
@@ -9,11 +9,11 @@ export default function ContactSection() {
             <div className='relative w-full md:w-1/2 h-full z-10  rounded-2xl rounded-image'>
                 <img src={bg} alt='' className='w-[100%] h-[100%] object-cover rounded-2xl'/>
             </div>
-            <div className='absolute md:relative w-full md:w-1/2 h-full z-20 flex flex-col justify-center px-5'>
+            <div className='absolute md:relative w-full md:w-1/2 h-[90%] md:h-full z-20 flex flex-col justify-center px-5'>
             <div className=' h-fit mb-5'>
     <p className='text-xl font-junge'>Contact Us</p>
     <p className='text-3xl mb-2 font-italiana'>Get in Touch</p>
-    <p className='text-lg font-junge text-zinc-700'>Have a question or feedback? Feel free to reach out to us! Our team is here to assist you with any inquiries and provide support. Whether it's a product inquiry, partnership opportunity, or just to say hello, we'd love to hear from you!</p>
+    <p className='text-sm lg:text-lg font-junge text-zinc-700'>Have a question or feedback? Feel free to reach out to us! Our team is here to assist you with any inquiries and provide support. Whether it's a product inquiry, partnership opportunity, or just to say hello, we'd love to hear from you!</p>
 </div>
                 <div className='mt-5'>
                   <ContactForm /></div>
