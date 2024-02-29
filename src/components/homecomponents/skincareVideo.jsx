2f5aba013d02da927c5db34246a376b26bb309cb
tidzi17 <tidzi15110@gmail.com>
2024-02-29 20:47:04 +0100

    sd

--- src/components/homecomponents/skincareVideo.jsx
+++ src/components/homecomponents/skincareVideo.jsx
@@ -6,7 +6,7 @@ import bgVideo from '../../assets/media/commercevideo.mp4';
 function SkincareVideo() {
   return (
     <section className='w-full h-[50vh] md:h-[85vh] rounded-3xl mb-10'>
-        <video src={bgVideo} type="video/mp4" controls={false} autoPlay loop muted playsInline
+        <video src={bgVideo} type="video/mp4" controls={false} autoPlay={true} loop muted playsInline
          className="rounded-3xl w-[100%] h-[100%] object-cover" alt="Background" />
    {/*  <img src={bg} alt='' className='rounded-3xl w-[100%] h-[100%] object-cover'/> */}
     </section>
